5f0f7d52a9fc68c22c4025213fb22e02ee176fb9
hz <hedzrz@gmail.com>
2025-02-19 22:40:28 +0800

    pnpm fumadocs init openapi

--- lib/source.ts
+++ lib/source.ts
@@ -1,9 +1,11 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 import { i18n } from "./i18n";
+import { createOpenAPI } from "fumadocs-openapi/server";
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
     i18n: i18n
 });
+export const openapi = createOpenAPI();
