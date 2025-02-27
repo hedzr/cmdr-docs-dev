e3a81d149d69261dd96d6e7b1b3fa6d078d80324
hz <hedzrz@gmail.com>
2025-02-28 07:25:18 +0800

    improved flipping component: support flipping page for blog index and content page by keyboard arrow keys

--- components/kb-page-flip.tsx
+++ components/kb-page-flip.tsx
@@ -1,29 +1,63 @@
 "use client";
 
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function HandlingKeyboardLeftAndRight({ dummy }: { dummy?: string }) {
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.key === "ArrowRight") {
-                event.preventDefault();
-                console.log('onKeyDown, right');
-                document.body.querySelector("a > div > svg.lucide-chevron-right")?.parentElement?.parentElement?.click();
-            } else if (event.key === "ArrowLeft") {
-                event.preventDefault();
-                console.log('onKeyDown, left');
-                document.body.querySelector("a > div > svg.lucide-chevron-left")?.parentElement?.parentElement?.click();
+export default function HandlingKeyboardLeftAndRight({
+  dummy,
+}: {
+  dummy?: string;
+}) {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        event.preventDefault();
+        console.log("onKeyDown, right");
+        const q = document.body;
+        const e = q.querySelector("a > div > svg.lucide-chevron-right");
+        if (e) e.parentElement?.parentElement?.click();
+        else {
+          const el = q.querySelector(
+            "ul.pagination > li.next:not(disabled) > a"
+          );
+          if (el) { // @ts-ignore
+            el.click();
+          }
+          else {
+            const el = q.querySelector(".next.right > a");
+            if (el) { // @ts-ignore
+              el.click();
             }
-        };
-        window.addEventListener("keydown", handleKeyDown);
+          }
+        }
+      } else if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        console.log("onKeyDown, left");
+        const q = document.body;
+        const e = q.querySelector("a > div > svg.lucide-chevron-left");
+        if (e) e.parentElement?.parentElement?.click();
+        else {
+          const el = q.querySelector(
+            "ul.pagination > li.previous:not(disabled) > a"
+          );
+          if (el) { // @ts-ignore
+            el.click();
+          }
+          else {
+            const el = q.querySelector(".prev.left > a");
+            if (el) { // @ts-ignore
+              el.click();
+            }
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
 
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
-    return (
-        <></>
-    );
+  return <></>;
 }
