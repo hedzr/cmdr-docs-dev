924f379282a2fbf22aaf61530610adf656786bb4
hz <hedzrz@gmail.com>
2025-03-02 13:01:14 +0800

    fix instrumentation.ts again III

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,7 +2,7 @@ import getConfig from "next/config";
 
 export async function register() {
   const config = getConfig();
-  if ('serverRuntimeConfig' in config) {
+  if (config && 'serverRuntimeConfig' in config) {
     const {serverRuntimeConfig, publicRuntimeConfig} = getConfig();
     if (serverRuntimeConfig && publicRuntimeConfig) {
       console.log(` + +++ [inspectInfo] __dirname: ${serverRuntimeConfig.PROJECT_ROOT}, serverRuntimeConfig: `,
