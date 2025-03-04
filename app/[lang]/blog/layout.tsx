929c0413c6c35e1f1f891c07ac651efd3fcf8dce
hz <hedzrz@gmail.com>
2025-03-04 08:16:24 +0800

    add exist test for 'content/blog/file-rec.mdx'

--- app/[lang]/blog/layout.tsx
+++ app/[lang]/blog/layout.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren, ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/layout/footer";
 import getConfig from "next/config";
+import path from "path";
+import * as fs from "node:fs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,10 @@ export default function BlogLayout({
   children: ReactNode; // PropsWithChildren;
 }) {
   const { serverRuntimeConfig } = getConfig();
-  console.log(`--- [BlogLayout] cwd: ${process.cwd()}, __dirname: ${ serverRuntimeConfig.PROJECT_ROOT }, serverRuntimeConfig.path: ${serverRuntimeConfig.path}`);
+  // const cwd1 = serverRuntimeConfig.cwd();
+  const cwd2 = process.cwd();
+  const o = fs.existsSync(path.join(cwd2,"content/blog/file-rec.mdx"));
+  console.log(`--- [BlogLayout] cwd: ${cwd2} | 'content/blog/file-rec.mdx': ${o}, __dirname: ${ serverRuntimeConfig.PROJECT_ROOT }, serverRuntimeConfig.path: ${serverRuntimeConfig.path}`);
   return (
     <div
       className={`flex flex-col items-start justify-center pt-8 pb-10 md:w-[87%] mx-auto ${geistSans.variable} ${geistMono.variable} antialiased`}
