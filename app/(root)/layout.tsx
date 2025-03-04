488e99e08802ea844557cb6a0a4513941463eb4c
hz <hedzrz@gmail.com>
2025-03-04 19:51:24 +0800

    remove unused (root)/layout.tsx

--- app/(root)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-import "../global.css";
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
-}
